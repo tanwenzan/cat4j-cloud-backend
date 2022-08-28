0aa41dd588842ce511652e6116b4a5f867b29bf9
zeroable <2011277648@qq.com>
2022-08-28 23:13:43 +0800

    公共模块注释统一

--- cat4j-cloud/cat4j-common/cat4j-common-core/src/main/java/cn/zeroable/cat4j/response/AbstractResponseResult.java
+++ cat4j-cloud/cat4j-common/cat4j-common-core/src/main/java/cn/zeroable/cat4j/response/AbstractResponseResult.java
@@ -1,6 +1,15 @@
 package cn.zeroable.cat4j.response;
 
 
+/**
+ * 响应结果集
+ * <br/> 类功能说明
+ *
+ * @author zeroable
+ * @version 2022/8/28 21:34
+ * @see
+ * @since 0.0.1
+ */
 public abstract class AbstractResponseResult implements ResponseResult {
 
     @Override
